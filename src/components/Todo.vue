5135a783bc49b2485fc2cdfadaf9a17af70875f0
ciobanuradu96 <52462743+ciobanuradu96@users.noreply.github.com>
2021-03-22 19:25:47 +0200

    added characther limitation

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="flex flex-col mx-auto mt-24 content-center text-center">
+  <div class="flex flex-col items-center m-auto mt-24 text-center">
     <p class="font-bold text-4xl text-red-400">To Do</p>
-    <div class="flex flex-row items-center">
+    <div class="text-right mr-12 mt-2 text-red-300 text-md m-auto">
+      {{ totalChar }}/{{ charLimit }}
+    </div>
+    <div class="flex flex-row items-center m-auto">
       <textarea
         id="newTodo"
         cols="40"
         rows="2"
-        class="m-4 rounded-2xl text-2xl shadow-md bg-red-50"
+        class="mx-4 mb-4 mt-2 rounded-2xl text-2xl py-1 px-2 shadow-md bg-red-50"
         v-model="newTodo"
+        @keyup.enter="addTodo(newTodo)"
       ></textarea>
       <button
         type="button"
@@ -17,38 +21,50 @@
         +
       </button>
     </div>
-    <div
+    <TodoItem
       v-for="(todoItem, index) in todoList"
       :key="index"
-      class="flex flex-row items-center mx-auto"
-    >
-      <div
-        class="m-2 text-xl bg-red-400 text-white p-3 rounded-lg shadow-md w-96 min-w-min cursor-pointer"
-      >
-        {{ todoItem }}
-      </div>
-      <button
-        class="bg-red-500 px-3 py-1 shadow-md rounded-3xl text-xl font-extrabold text-center font-mono text-white hover:text-red-500 hover:bg-white"
-        @click="deleteTodo(index)"
-      >
-        -
-      </button>
-    </div>
+      :todoItem="todoItem"
+      :index="index"
+      @delete="deleteTodo"
+    />
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
 export default {
+  components: {
+    TodoItem,
+  },
   data() {
     return {
-      newTodo: "test",
+      newTodo: "",
       todoList: [],
+      charLimit: 80,
     };
+  },
+  computed: {
+    totalChar: function () {
+      return this.newTodo.length;
+    },
+  },
+  watch: {
+    totalChar: function (newValue) {
+      if (newValue > this.charLimit) {
+        this.newTodo = this.newTodo.slice(
+          0,
+          -(this.totalChar - this.charLimit)
+        );
+      }
+    },
   },
   methods: {
     addTodo(newTodo) {
       this.todoList.unshift(newTodo);
       this.newTodo = "";
+      this.totalChar = 0;
+      this.disabled = 0;
     },
     deleteTodo(index) {
       this.todoList.splice(index, 1);
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,31 @@
-<template></template>
+<template>
+  <div class="flex items-center content-center w-96">
+    <div
+      class="flex-grow m-2 text-xl bg-red-400 text-white rounded-lg shadow-md cursor-pointer overflow-y-auto"
+      @dblclick="deleteTodo(index)"
+    >
+      <p>{{ todoItem }}</p>
+    </div>
+    <div class="flex-none">
+      <button
+        class="bg-red-500 px-4 py-1 shadow-md rounded-3xl text-xl font-extrabold text-center font-mono text-white hover:text-red-500 hover:bg-white"
+        @click="deleteTodo(index)"
+      >
+        -
+      </button>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  props: ["todoItem", "index"],
+  methods: {
+    deleteTodo(index) {
+      this.$emit("delete", index);
+    },
+  },
+};
 </script>
 
 <style>
